8ba5f02cc2e2f302cb6f6ee1a98da65d4fcd7dc3
lon5948 <a0988282303@gmail.com>
2023-04-27 01:06:23 +0800

    handshake

--- hw5/quic_client.py
+++ hw5/quic_client.py
@@ -1,19 +1,28 @@
+import socket
+
 class QUICClient:
-    def connect(socket_addr: tuple[str, int]):
-        """connect to the specific server"""
+    def __init__(self):
+        self.socket_ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # connect to the specific server
+    def connect(self, socket_addr): 
+        hello = "Client Hello" 
+        self.socket_.sendto(hello.encode("utf-8"), socket_addr)
+        hello_ack, server_addr = self.socket_.recvfrom(1024)
+        print("Message:", hello_ack.decode("utf-8"), "from", server_addr)
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    # call this method to send data, with non-reputation stream_id
+    def send(self, stream_id: int, data: bytes):
         pass
     
-    def send(stream_id: int, data: bytes):
-        """call this method to send data, with non-reputation stream_id"""
+    # receive a stream, with stream_id
+    def recv(self): # -> tuple[int, bytes] stream_id, data
         pass
     
-    def recv() -> tuple[int, bytes]: # stream_id, data
-        """receive a stream, with stream_id"""
-        pass
-    
-    def close():
-        """close the connection and the socket"""
-        pass
+    # close the connection and the socket
+    def close(self):
+        self.socket_.close()
 
 if __name__ == "__main__":
     client = QUICClient()
--- hw5/quic_server.py
+++ hw5/quic_server.py
@@ -1,24 +1,33 @@
+import socket
+
 class QUICServer:
-    def listen(socket_addr: tuple[str, int]):
-        """this method is to open the socket"""
+    def __init__(self):
+        self.socket_ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # this method is to open the socket 
+    def listen(self, socket_addr):
+        self.socket_.bind(socket_addr)
+
+    # this method is to indicate that the client can connect to the server now
+    def accept(self):
+        hello, client_addr = self.socket_.recvfrom(1024)
+        print("Message:", hello.decode("utf-8"), "from", client_addr)
+        hello_ack = "Hello ACK"
+        self.socket_.sendto(hello_ack.encode("utf-8"), client_addr)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client_socket.bind(client_addr)
+    
+    # call this method to send data, with non-reputation stream_id
+    def send(self, stream_id: int, data: bytes):
         pass
     
-    def accept():
-        """this method is to indicate that the client
-        can connect to the server now"""
+    # receive a stream, with stream_id
+    def recv(self): # -> tuple[int, bytes] stream_id, data
         pass
     
-    def send(stream_id: int, data: bytes):
-        """call this method to send data, with non-reputation stream_id"""
-        pass
-    
-    def recv() -> tuple[int, bytes]: # stream_id, data
-        """receive a stream, with stream_id"""
-        pass
-    
-    def close():
-        """close the connection and the socket"""
-        pass
+    # close the connection and the socket
+    def close(self):
+        self.socket_.close()
 
 if __name__ == "__main__":
     server = QUICServer()
