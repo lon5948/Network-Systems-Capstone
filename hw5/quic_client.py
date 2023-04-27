4b845eed771f1bbaeec5763309ba1a788cb53f60
lon5948 <a0988282303@gmail.com>
2023-04-28 00:14:51 +0800

    first commit

--- hw5/quic_client.py
+++ hw5/quic_client.py
@@ -1,24 +1,64 @@
-import socket
+import socket, struct
+import threading, heapq
+
+def create_stream(stream_id, data, client_addr, server_addr):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(client_addr)
+    length = len(data)
+    """ streamID, offset, finish, payload"""
+    for i in range(0, length, 1500):
+        if i + 1500 < length:
+            frame = struct.pack("iii1500s",stream_id, i, 0, data[i:i+1500])
+        else:
+            frame = struct.pack("iii1500s",stream_id, i, 1, data[i:length])
+        s.sendto(frame, server_addr)
+        ack, s_addr = s.recvfrom(2048)
+        if ack.decode("utf-8") != "ACK":
+            print("[ERROR] ACK FROM SERVER IS INCORRECT")
+            exit(0)
+
+def recv_packet(client_addr):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(client_addr)
+    flag = False
+    frame_num = 0
+    total_num = 0
+    d = dict()
+    while True:
+        frame, c_addr = s.recvfrom(2048)
+        stream_id, offset, finish, payload = struct.unpack("iii1500s", frame)
+        frame_num += 1
+        total_num = max(total_num, offset/1500 + 1)
+        d[offset] = payload
+        if finish == 1:
+            flag = True
+        if flag and frame_num == total_num:
+            break
+    data = ""
+    for i in range(total_num):
+        data += d[i*1500]
+    return (stream_id, data)
 
 class QUICClient:
-    def __init__(self):
-        self.socket_ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
     # connect to the specific server
     def connect(self, socket_addr): 
+        self.addr = socket_addr
+        self.socket_ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         hello = "Client Hello" 
         self.socket_.sendto(hello.encode("utf-8"), socket_addr)
-        hello_ack, server_addr = self.socket_.recvfrom(1024)
+        hello_ack, server_addr = self.socket_.recvfrom(2048)
+        self.server_addr = server_addr
         print("Message:", hello_ack.decode("utf-8"), "from", server_addr)
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     # call this method to send data, with non-reputation stream_id
     def send(self, stream_id: int, data: bytes):
-        pass
+        s_th = threading.Thread(target=create_stream, args=(stream_id, data, self.addr, self.server_addr))
+        s_th.start()
     
     # receive a stream, with stream_id
     def recv(self): # -> tuple[int, bytes] stream_id, data
-        pass
+        r_th = threading.Thread(target=recv_packet, args=(self.addr))
+        r_th.start()
     
     # close the connection and the socket
     def close(self):
--- hw5/quic_server.py
+++ hw5/quic_server.py
@@ -1,29 +1,69 @@
-import socket
+import socket, struct
+import threading, heapq
+
+def create_stream(stream_id, data, server_addr, client_addr):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(server_addr)
+    length = len(data)
+    """ streamID, offset, finish, payload"""
+    for i in range(0, length, 1500):
+        if i + 1500 < length:
+            frame = struct.pack("iii1500s",stream_id, i, 0, data[i:i+1500])
+        else:
+            frame = struct.pack("iii1500s",stream_id, i, 1, data[i:length])
+        s.sendto(frame, client_addr)
+        ack, c_addr = s.recvfrom(2048)
+        if ack.decode("utf-8") != "ACK":
+            print("[ERROR] ACK IS INCORRECT")
+            exit(0)
+
+def recv_packet(server_addr):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(server_addr)
+    flag = False
+    frame_num = 0
+    total_num = 0
+    d = dict()
+    while True:
+        frame, c_addr = s.recvfrom(2048)
+        stream_id, offset, finish, payload = struct.unpack("iii1500s", frame)
+        frame_num += 1
+        total_num = max(total_num, offset/1500 + 1)
+        d[offset] = payload
+        if finish == 1:
+            flag = True
+        if flag and frame_num == total_num:
+            break
+    data = ""
+    for i in range(total_num):
+        data += d[i*1500]
+    return (stream_id, data)
+
 
 class QUICServer:
-    def __init__(self):
-        self.socket_ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
     # this method is to open the socket 
     def listen(self, socket_addr):
+        self.addr = socket_addr
+        self.socket_ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket_.bind(socket_addr)
-
+    
     # this method is to indicate that the client can connect to the server now
     def accept(self):
-        hello, client_addr = self.socket_.recvfrom(1024)
+        hello, client_addr = self.socket_.recvfrom(2048)
         print("Message:", hello.decode("utf-8"), "from", client_addr)
         hello_ack = "Hello ACK"
         self.socket_.sendto(hello_ack.encode("utf-8"), client_addr)
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        client_socket.bind(client_addr)
+        self.client_addr = client_addr
     
     # call this method to send data, with non-reputation stream_id
     def send(self, stream_id: int, data: bytes):
-        pass
-    
+        s_th = threading.Thread(target=create_stream, args=(stream_id, data, self.addr, self.client_addr))
+        s_th.start()
+        
     # receive a stream, with stream_id
     def recv(self): # -> tuple[int, bytes] stream_id, data
-        pass
+        r_th = threading.Thread(target=recv_packet, args=(self.addr))
+        r_th.start()
     
     # close the connection and the socket
     def close(self):
