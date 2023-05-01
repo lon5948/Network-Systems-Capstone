3a4bfce9935d83cb62f15d6b4bd397ed79fbf314
lon5948 <a0988282303@gmail.com>
2023-05-01 17:29:57 +0800

    quic_server error control

--- hw5/quic_server.py
+++ hw5/quic_server.py
@@ -1,10 +1,13 @@
 import socket, struct
 import threading, heapq
 
+BUFFER_SIZE = 4096
+
 class QUICServer:
     def __init__(self):
-        self.send_window_size = 10
-
+        self.send_buffer = dict()
+        self.recv_buffer = dict()
+        self.congestion_window = 2
 
     # this method is to open the socket 
     def listen(self, socket_addr):
@@ -19,68 +22,70 @@ class QUICServer:
         hello_ack = "Hello ACK"
         self.socket_.sendto(hello_ack.encode("utf-8"), client_addr)
         self.client_addr = client_addr
+        th = threading.Thread(target=self.func)
+        th.start()
     
-    def create_stream(self, stream_id, data):
-        length = len(data)
-        windows = 0
-        packet_id = 0
-        ack_id = 0
-        i = 0
-        packets = dict()
-        """ streamID, packet_id, offset, finish, payload """
+    def func(self):
+        """ 
+        [SEND BUFFER] { stream_id : { 'data': data, 'next': offset, 'wait_ack': list() }}
+        [RECV BUFFER] { stream_id: { finish: True, total_num: 5, payload : { offset: "This is data." }}}
+        """
         while True:
-            if i < length and windows < self.send_window_size:
-                if i + 1500 < length:
-                    stream_frame = struct.pack("iiii1500s",stream_id, packet_id, i, 0, data[i:i+1500])
-                else:
-                    stream_frame = struct.pack("iiii1500s",stream_id, packet_id, i, 1, data[i:length])
-                packets[packet_id] = stream_frame
-                self.socket_.sendto(stream_frame, self.client_addr)
-                i += 1500
-                packet_id += 1
-                windows += 1
-            if windows == self.send_window_size:
-                ack_frame, c_addr = self.socket_.recvfrom(2048)
-                id, ack = struct.unpack("i3s", ack_frame)
-                if ack.decode("utf-8") != "ACK":
-                    print("[ERROR] ACK IS INCORRECT")
-                    exit(0)
-                if id == ack_id:
-                    ack_id += 1
-                    windows -= 1
-                else:
-                    self.socket_.sendto(packets[packet_id], self.client_addr)
-    
+            for stream_id, data in self.send_buffer.items():
+                for i in range(self.congestion_window):
+                    if data['next'] == len(data['data']):
+                        offset = data['wait_ack'][0]
+                        data['wait_ack'].remove(offset)
+                    else:
+                        offset = data['next']
+                    next_offset = offset + 1500
+                    if next_offset > len(data['data']):
+                        next_offset = len(data['data'])
+                    # stream_id, type, offset, finish, payload
+                    stream_frame = struct.pack("i3sii1500s",stream_id, "STR", offset, 0, data['data'][offset:next_offset])
+                    self.socket_.sendto(stream_frame, self.client_addr)
+                    self.send_buffer[stream_id]['wait_ack'].append(offset)
+                    if next_offset != len(data['data']):
+                        data['next'] = data['next'] + 1500
+            
+            self.socket_.settimeout(3)
+            while True:
+                try:
+                    frame, c_addr = self.socket_.recvfrom(2048)
+                except socket.timeout as e:
+                    self.socket_.gettimeout()   
+                    break
+                stream_id, type, offset, finish, payload = struct.unpack("i3sii1500s", frame)
+                if type == "STR":
+                    if stream_id not in self.recv_buffer:
+                        self.recv_buffer[stream_id] = {'finish':False, 'total_num':0, 'payload':dict()}
+                    if finish == 1:
+                        self.recv_buffer[stream_id]['finish'] = True
+                        self.recv_buffer[stream_id]['total_num'] = offset/1500 + 1
+                    self.recv_buffer[stream_id]['payload'][offset] = payload
+                    ack = struct.pack("i3sii1500s",stream_id, "ACK", offset, 0, '0')
+                    self.socket_.sendto(ack, self.client_addr)
+                elif type == "ACK":
+                    count = self.send_buffer[stream_id]['wait_ack'].count(offset)
+                    for c in count:
+                        self.send_buffer[stream_id]['wait_ack'].remove(offset)
+                    if len(self.send_buffer[stream_id]['wait_ack']) == 0:
+                        del self.send_buffer[stream_id]
+
     # call this method to send data, with non-reputation stream_id
     def send(self, stream_id: int, data: bytes):
-        s_th = threading.Thread(target=self.create_stream, args=(stream_id, data))
-        s_th.start()
-
-    def recv_packet(self):
-        flag = False
-        total_num = 0
-        d = dict()
-        while True:
-            stream_frame, c_addr = self.socket_.recvfrom(2048)
-            stream_id, packet_id, offset, finish, payload = struct.unpack("iiii1500s", stream_frame)
-            total_num = max(total_num, offset/1500 + 1)
-            d[offset] = payload
-            ack_frame = struct.pack("i3s", packet_id, "ACK")
-            self.socket_.sendto(ack_frame, self.client_addr)
-            if finish == 1:
-                flag = True
-            if flag and len(d) == total_num:
-                break
-        data = ""
-        for i in range(total_num):
-            data += d[i*1500]
-        return (stream_id, data)
+        self.send_buffer[stream_id] = {'payload':data, 'next':0, 'ack':list()}
     
     # receive a stream, with stream_id
     def recv(self): # -> tuple[int, bytes] stream_id, data
-        r_th = threading.Thread(target=self.recv_packet)
-        r_th.start()
-
+        for stream_id, data in self.recv_buffer.items():
+            if data['finish'] == True and len(data['payload']) == data['total_num']:
+                ret = ""
+                for i in range(data['total_num']):
+                    ret += data['payload'][i*1500]
+                del self.recv_buffer[stream_id]
+                return (stream_id, ret)
+                    
     # close the connection and the socket
     def close(self):
         self.socket_.close()
