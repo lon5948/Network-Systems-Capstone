ac211d614aefc72eacc8cbd4a47d6c8ecd4996d8
lon5948 <a0988282303@gmail.com>
2023-05-25 21:38:54 +0800

    fix HTTP/3 bugs

--- hw6/demo/my/http_3_0_client.py
+++ hw6/demo/my/http_3_0_client.py
@@ -2,17 +2,17 @@ import time, threading
 from collections import deque
 from my.QUIC.quic_client import QUICClient 
 
-def recv_response(quic_client, response, path, server_ip, server_port):
+def recv_response(quic_client, responses, path, server_ip, server_port, stream_id):
     test = 0
     request = f"GET {path} http {server_ip}:{server_port}"
     request_length = len(request)
     header = (1).to_bytes(1, byteorder='big') + request_length.to_bytes(4, byteorder='big')
     h_frame = header + request.encode()
-    quic_client.send(response.stream_id, h_frame, end=True)
-    while response.complete == False:
+    quic_client.send(stream_id, h_frame, end=True)
+    while responses[stream_id].complete == False:
         #print("-------wait to receive data---------")
         time.sleep(0.2)
-        stream_id, data, flags = quic_client.recv()
+        sid, data, flags = quic_client.recv()
         #print(data)
         types = data[0]
         length = int.from_bytes(data[1:5], byteorder='big')
@@ -24,19 +24,19 @@ def recv_response(quic_client, response, path, server_ip, server_port):
                 sid, d, flags = quic_client.recv()
                 payload += d
                 #print("again: ", len(payload), length)
-            response.contents.append(payload)
-            response.complete = flags
+            responses[sid].contents.append(payload)
+            responses[sid].complete = flags
             #print("complete: ", flags)
             test += len(payload)
-            print(response.stream_id, "total length: ", test)
+            print(sid, "total length: ", test)
         elif types == 1:
             #print("header", len(payload), length)
             #print("get header frame")
             payload = payload.decode()
             #print(payload)
             payload = payload.split('\r\n')
-            response.status = payload[0]
-            response.headers = {
+            responses[sid].status = payload[0]
+            responses[sid].headers = {
                 payload[1].split(':')[0].lower(): payload[1].split(':')[1],
                 payload[2].split(':')[0].lower(): payload[2].split(':')[1],
             }
@@ -47,19 +47,19 @@ class HTTPClient(): # For HTTP/3
         self.num = 0
         self.stream_id = 1
         self.threads = []
-        self.responses = []
+        self.responses = {}
 
     def get(self, url, headers=None):
         server_ip, server_port, path = self.parse_url(url)
         if path == '/':
             self.quic_client.connect((server_ip, server_port))
         response = Response(self.stream_id)
-        self.responses.append(response)
+        self.responses[self.stream_id] = response
         print(self.stream_id)
-        self.stream_id += 2
-        thread = threading.Thread(target=recv_response, args=(self.quic_client, response, path, server_ip, server_port))
+        thread = threading.Thread(target=recv_response, args=(self.quic_client, self.responses, path, server_ip, server_port, self.stream_id))
         self.threads.append(thread) 
         thread.start()
+        self.stream_id += 2
         return response
     
     def parse_url(self, url):
@@ -101,6 +101,5 @@ class Response():
             time.sleep(0.01)
         if len(self.contents) == 0: # contents is a buffer, busy waiting for new content
             return None
-        print("get content")
         content = self.contents.popleft() # pop content from deque
         return content # the part content of the HTTP response body
