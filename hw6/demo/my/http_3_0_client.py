a275e56bf6e9f96cb9c6d8b8525ea8e3f16ef9bf
lon5948 <a0988282303@gmail.com>
2023-05-25 16:02:39 +0800

    fix HTTP/3 bugs

---
+++ hw6/demo/my/http_3_0_client.py
@@ -0,0 +1,99 @@
+import time
+from collections import deque
+from QUIC.quic_client import QUICClient 
+
+class HTTPClient(): # For HTTP/3
+    def __init__(self) -> None:
+        self.quic_client = QUICClient()
+        self.num = 0
+        self.stream_id = 1
+
+    def get(self, url, headers=None):
+        print("get", url)
+        server_ip, server_port, path = self.parse_url(url)
+        if path == '/':
+            self.quic_client.connect((server_ip, server_port))
+        request = f"GET {path} http {server_ip}:{server_port}"
+        request_length = len(request)
+        header = (1).to_bytes(1, byteorder='big') + request_length.to_bytes(4, byteorder='big')
+        h_frame = header + request.encode()
+        self.quic_client.send(self.stream_id, h_frame, end=True)
+        print("request send")
+        response = Response(self.stream_id)
+        self.stream_id += 2
+        while response.complete == False:
+            print("------wait to receive packet-------")
+            stream_id, data, flags = self.quic_client.recv()
+            print("[REICEIVE]]", data)
+            types = data[0]
+            length = int.from_bytes(data[1:5], byteorder='big')
+            payload = data[5:5+length]
+            '''
+            while len(payload) != length:
+                payload += self.quic_client.recv(length-len(payload))
+            '''
+            if types == 0:
+                print("data frame")
+                response.contents.append(payload)
+                if flags == True:
+                    response.complete = True
+            elif types == 1:
+                print("header frame")
+                payload = payload.decode()
+                payload = payload.split('\r\n')
+                response.status = payload[0]
+                response.headers = {
+                    payload[1].split(':')[0].lower(): payload[1].split(':')[1],
+                    payload[2].split(':')[0].lower(): payload[2].split(':')[1],
+                }
+        print("get response finish")
+        self.num += 1
+        if self.num == 4:
+            self.quic_client.close()
+        return response
+    
+    def parse_url(self, url):
+        if url.startswith("http://"):
+            url = url[7:]
+        elif url.startswith("https://"):
+            url = url[8:]
+        url = url.split(':')
+        ip = url[0]
+        port = url[1].split('/')[0]
+        path = url[1][len(port):]
+        return ip, int(port), path
+    
+class Response():
+    def __init__(self, stream_id, headers = {}, status = "Not yet") -> None:
+        self.stream_id = stream_id
+        self.headers = headers
+        self.status = status
+        self.body = b""
+        self.contents = deque()
+        self.complete = False
+        
+    def get_headers(self):
+        begin_time = time.time()
+        while self.status == "Not yet":
+            if time.time() - begin_time > 5:
+                return None
+        return self.headers
+    
+    def get_full_body(self): # used for handling short body
+        begin_time = time.time()
+        while not self.complete:
+            if time.time() - begin_time > 5:
+                return None
+        if len(self.body) > 0:
+            return self.body
+        while len(self.contents) > 0:
+            self.body += self.contents.popleft()
+        return self.body # the full content of HTTP response body
+    
+    def get_stream_content(self): # used for handling long body
+        begin_time = time.time()
+        while len(self.contents) == 0: # contents is a buffer, busy waiting for new content
+            if self.complete or time.time() - begin_time > 5: # if response is complete or timeout
+                return None
+        content = self.contents.popleft() # pop content from deque
+        return content # the part content of the HTTP response body
--- hw6/demo/my/http_3_0_server.py
+++ hw6/demo/my/http_3_0_server.py
@@ -1,13 +1,85 @@
 from QUIC.quic_server import QUICServer
+import threading, os
 
+CHUNK_SIZE = 4096
+
+def send_response(quic_server, stream_id, data, finish, directory):
+    types = data[0]
+    request_length = int.from_bytes(data[1:5], byteorder='big')
+    request = data[5:5+request_length].decode().split(' ')
+    request_path = request[1]
+    print("request path", request_path)
+    if request_path == "/":
+        d_payload = "<html><header></header><body>"
+        files = os.listdir(directory)
+        for i in range(3):
+            d_payload += f"<a href='/static/{files[i]}'>{files[i]}</a>"
+            if i == 2:
+                break
+            d_payload += "<br/>"
+        d_payload += "</body></html>"
+        d_frame = (0).to_bytes(1, byteorder='big') + len(d_payload).to_bytes(4, byteorder='big') + d_payload.encode()
+
+        h_payload = f"200 OK\r\nContent-Type:text/html\r\nContent-Length:{len(d_payload)}"
+        h_frame = (1).to_bytes(1, byteorder='big') + len(h_payload).to_bytes(4, byteorder='big') + h_payload.encode()
+
+        quic_server.send(stream_id, h_frame, end=True)
+        quic_server.send(stream_id, d_frame, end=True)
+        print("response send")
+
+    elif request_path.startswith('/static'):
+        full_path = directory + request_path[7:]
+        file_size = os.path.getsize(full_path)
+
+        h_payload = f"200 OK\r\nContent-Type:text/html\r\nContent-Length:{file_size}"
+        h_frame = (1).to_bytes(1, byteorder='big') + len(h_payload).to_bytes(4, byteorder='big') + h_payload.encode()
+        quic_server.send(stream_id, h_frame, end=True)
+        print("header response send")
+        with open(full_path, "rb") as file:
+            flag = True
+            complete = False
+            while complete == False:
+                if flag:
+                    d_payload = file.read(CHUNK_SIZE)
+                    flag = False
+                else:
+                    d_payload = d_payload_next
+                d_payload_next = file.read(CHUNK_SIZE)
+                if not d_payload_next:
+                    complete = True
+                d_frame = (0).to_bytes(1, byteorder='big') + len(d_payload).to_bytes(4, byteorder='big') + d_payload
+                quic_server.send(stream_id, d_frame, end=complete)
+                print("send file")
+        print("file response finish")
+    else:
+        d_payload = "<html><header></header><body></body></html>"
+        d_frame = (0).to_bytes(1, byteorder='big') + len(d_payload).to_bytes(4, byteorder='big') + d_payload.encode()
+        h_payload = f"404 Not Found\r\nContent-Type:text/html\r\nContent-Length:{len(d_payload)}"
+        h_frame = (1).to_bytes(1, byteorder='big') + len(h_payload).to_bytes(4, byteorder='big') + h_payload.encode()
+        quic_server.send(stream_id, h_frame, end=True)
+        quic_server.send(stream_id, d_frame, end=True)
+        
 class HTTPServer():
     def __init__(self, host="127.0.0.1", port=8080) -> None:
         self.quic_server = QUICServer()
-        self.sockaddr = (host, port)
+        self.server_addr = (host, port)
+        self.threads = []
 
     def run(self):
-        self.quic_server.listen(self.sockaddr)
+        print("start to run")
+        self.quic_server.listen(self.server_addr)
         self.quic_server.accept()
+        print("accept finish")
+        while True:
+            stream_id, data, finish = self.quic_server.recv()
+            print("receive request")
+            if not data:
+                break
+            thread = threading.Thread(target=send_response, args=(self.quic_server, stream_id, data, finish, self.directory))
+            self.threads.append(thread)
+            thread.start()
+        for th in self.threads:
+            th.join()
 
     def set_static(self, path):
         self.directory = path
