90e03350bbb2b2a6bdb4f1bff1c1584c43a86938
lon5948 <a0988282303@gmail.com>
2023-06-06 04:44:04 +0800

    update controller.py

--- hw7/controller.py
+++ hw7/controller.py
@@ -2,16 +2,17 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ofproto_v1_5
 from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet, ether_types
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
 
-class Controller(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+class SimpleSwitch15(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_5.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(Controller, self).__init__(*args, **kwargs)
-        # initialize mac address table.
+        super(SimpleSwitch15, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -20,17 +21,18 @@ class Controller(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # install the table-miss flow entry.
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # construct flow_mod message and send it.
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                 match=match, instructions=inst)
         datapath.send_msg(mod)
@@ -41,41 +43,37 @@ class Controller(app_manager.RyuApp):
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-
-        # get Datapath ID to identify OpenFlow switches.
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
-
-        # analyse the received packets using the packet library.
-        pkt = packet.Packet(msg.data)
-        eth_pkt = pkt.get_protocol(ethernet.ethernet)
-        dst = eth_pkt.dst
-        src = eth_pkt.src
-
-        # get the received port number from packet_in message.
         in_port = msg.match['in_port']
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        # Default table, go to filter_table_1
-        match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
-        self.add_flow(datapath, 1, match, actions)
+        # filter_table_1
+        if eth.ethertype == ether_types.ETH_TYPE_ICMP:
+            # filter_table_2
+            in_port = msg.match['in_port']
+            if in_port == 3 or in_port == 4:
+                match = parser.OFPMatch(in_port=in_port)
+                actions = []
+                self.add_flow(datapath, 2, match, actions)
+            else:
+                # forward table
+                match = parser.OFPMatch()
+                actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+                self.add_flow(datapath, 1, match, actions)
+        else:
+            # forward table
+            match = parser.OFPMatch()
+            actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+            self.add_flow(datapath, 1, match, actions)
+        '''
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
 
-        # Filter_table_1, check if packet is ICMP
-        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
-                                ip_proto=1)
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
-        self.add_flow(datapath, 2, match, actions)
-
-        # Filter_table_2, drop packets from target_ports, forward other packets
-        match = parser.OFPMatch(in_port=3)
-        actions = []
-        self.add_flow(datapath, 3, match, actions)
-
-        match = parser.OFPMatch(in_port=4)
-        actions = []
-        self.add_flow(datapath, 3, match, actions)
-
-        # Forward Table
-
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  match=match, actions=actions, data=data)
+        '''
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  match=match, actions=actions)
+        datapath.send_msg(out)
