0a3f2fc1abea6046ba8b85bb2c837aa0332b5472
chuchunchi <chuchun1231@gmail.com>
2023-06-06 15:08:11 +0800

    update

--- hw7/controller.py
+++ hw7/controller.py
@@ -8,11 +8,11 @@ from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 
 
-class SimpleSwitch15(app_manager.RyuApp):
+class Controller(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_5.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch15, self).__init__(*args, **kwargs)
+        super(Controller, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -47,26 +47,30 @@ class SimpleSwitch15(app_manager.RyuApp):
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
+        
+        dpid = datapath.id
+        src = eth.src
+        dst = eth.dst
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
         # filter_table_1
-        if eth.ethertype == ether_types.ETH_TYPE_ICMP:
+        if eth.ethertype == ether_types.ETH_TYPE_IP and pkt.protocol(ipv4.ipv4) == inet.IPPROTO_ICMP:
             # filter_table_2
-            in_port = msg.match['in_port']
             if in_port == 3 or in_port == 4:
                 match = parser.OFPMatch(in_port=in_port)
                 actions = []
                 self.add_flow(datapath, 2, match, actions)
             else:
                 # forward table
-                match = parser.OFPMatch()
+                match = parser.OFPMatch(in_port=msg.match['in_port'])
                 actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
                 self.add_flow(datapath, 1, match, actions)
         else:
             # forward table
-            match = parser.OFPMatch()
+            match = parser.OFPMatch(in_port=msg.match['in_port'])
             actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
             self.add_flow(datapath, 1, match, actions)
-        '''
+        
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
@@ -76,4 +80,5 @@ class SimpleSwitch15(app_manager.RyuApp):
         '''
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   match=match, actions=actions)
+        '''
         datapath.send_msg(out)
