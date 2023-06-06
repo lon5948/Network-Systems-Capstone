90482965a0fb2a0ca686889e548dccb2f2aa5758
chuchunchi <chuchun1231@gmail.com>
2023-06-06 23:47:03 +0800

    topo2 ok

--- hw7/topo2.py
+++ hw7/topo2.py
@@ -2,9 +2,6 @@ from mininet.topo import Topo
 
 class MyTopo(Topo):
     def build(self):
-        #Add controller
-        controller = self.addController('c1', controller=RemoteController, ip='', port=6633)
-
         # Add switches
         switch = self.addSwitch('s2')
         
@@ -15,6 +12,7 @@ class MyTopo(Topo):
             hosts.append(host)
         
         # Add links
-        self.addLink(controller, switch)
         for i in range(4):
-            self.addLink(hosts[i+4], switch)
+            self.addLink(hosts[i], switch)
+
+topos = {'mytopo': (lambda: MyTopo())}
